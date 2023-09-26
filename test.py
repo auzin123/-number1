64c7a5d559df4c873863129a74bbbefba2453ac1
auzin123 <145466207+auzin123@users.noreply.github.com>
2023-09-26 17:42:51 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
-age = int(input("Введите возраст: "))
+name = "Илья Муромец"
 
-if age >= 18:
-    print("Билет ваш")
-elif age == 17:
-    print(":)")
-else:
-    if age >99:
-        print ("Ты слишком стар")
-    print("Мал ещё")
+way_left = False
+way_center = False
+way_right = False
+
+while way_left == False or way_center == false or way_right == False:
+    
+    print(name, "встретил на распутье трех дорог горючий камень с надписью")
+    print("Налево поехать - уьитым быть")
+    print("Прямо поехать - женатым быть")
+    print("Направо поехать =-  богатым быть")
+
+    way = input("Куда поехать?")
+    if way == "налево":
+        if way_left == False:
+            print(name, "убит!")
+            way_left = True
+        else:
+            print(name, "уже был на левой дороге")
+    elif way == "прямо":
+        print(name, "женат")
+        way_center = True
+    elif way == "направо":
+        print(name, "богат!")
+        way_right = True
+    else:
+        print("Нет такой дороги")
