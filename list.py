3a885a38a23c5374a1eb508465776c4f588938cc
auzin123 <145466207+auzin123@users.noreply.github.com>
2023-12-12 17:45:41 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+# append -  присоеденяет в конец
+# pop - удаляет или возвращает элемент
+# in - есть ли элемент в списке
+# sort - сортируем список
+# сортируем только однородные (type) списки
+
+
+from  random import randint
+random_digits = []
+for i in range(10):
+    random_digits.append(randint(0, 9))
+
+
+print(random_digits)
