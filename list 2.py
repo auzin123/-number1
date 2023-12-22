5bdbd7da0ae421a5bf6f10169e81c59063717abc
auzin123 <145466207+auzin123@users.noreply.github.com>
2023-12-22 17:47:56 +0300

    Add files via upload

--- list 2.py
+++ list 2.py
@@ -7,12 +7,14 @@ def get_hero(
         level=1,
         xp=0, 
         money=25, 
-        inventory="пусто"
+        inventory=" "
         ) -> list:
     """ Возвращает список - героя """
     if not name:
         names = ("Роман", "Валера", "Акакий", "Мао")
         name = choice(names)
+    if not inventory:
+        inventory = None
     return [name, hp, level, xp, money, inventory]
 
 def show_hero(hero: list) -> None:
@@ -27,13 +29,15 @@ def show_hero(hero: list) -> None:
     print("-" * 15)
 
 
-def visit_shop(hero, shop_items):
+def visit_shop(hero: list, shop_items: list):
     """ Выводит на экран текст магазина """
     """ Выводит на экран пронумерованные опции """
     """ Предлагает герою выбор опции """
     """ Работает по выбранной опции """
     show_hero(player)
     print(f"{hero[0]} прихал в лавку.")
+    price = 10
+    print("Распродажа: все товары по 10 монет")
     print("1 - Купить товары")
     print("2 - Продать товары")
     print("3 - Выйти из лавки")
@@ -42,11 +46,24 @@ def visit_shop(hero, shop_items):
     if option == "1":
         for num, item in enumerate(shop_items, 1):
             print(f"{num} - {item}")
-        option = input("Введите номер товара чтобы его купить или введите 0 для отмены")
-        # TODO: купить товар по выбранной опции
+        print("0 -  Отмена")
+        print("-" * 15)
+        option = input("Введите номер товара: ")
+        print("-" * 15)
+        if int(option) > len(shop_items) or int(option) < 0:
+            print("неверная опция")
+        elif option == "0":
+            print("вы вышли")
+        else:
+            if hero[4] < price:
+                print(f"у {hero[0]} недостаточно денег")
+            else:
+                hero[4] -= price
+                hero[5].append(option) - 1
+                print(f"{hero[0]} купил {shop_items[int(option) - 1]} и теперь у него {hero[4]} денег")
+                print("-" * 15)
         
-
-
+        
 player = get_hero()
-shop_items = ["зелье лечения", "зелье лечения", "зелье копчения",]
+shop_items = ["зелье лечения", "новое оружие", "зелье копчения",]
 visit_shop(player, shop_items)
