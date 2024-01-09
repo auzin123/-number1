201417e427d3cd9ac49a1cc1e4a49bbf21327cd8
auzin123 <145466207+auzin123@users.noreply.github.com>
2024-01-09 16:58:13 +0300

    Add files via upload

---
+++ num 2.py
@@ -0,0 +1,32 @@
+def get_month_name(month):
+    if month == 3:
+        print("весна")
+    if month == 4:
+        print("весна")
+    if month == 5:
+        print("весна")
+    if month == 6:
+        print("лето")
+    if month == 7:
+        print("лето")
+    if month == 8:
+        print("лето")
+    if month == 9:
+        print("осень")
+    if month == 10:
+        print("осень")
+    if month == 11:
+        print("осень")
+    if month == 12:
+        print("зима")
+    if month == 1:
+        print("зима")
+    if month == 2:
+        print("зима")
+    if month < 1:
+        print("")
+    if month > 12:
+        print("")
+    return
+
+get_month_name(month=1)
